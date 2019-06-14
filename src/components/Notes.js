eb826661a17f986171a20d5af60716b9f480a804
Kirill <kiril.borushko@gmail.com>
2019-06-14 18:12:02 +0300

    added an event handler to get data from json-server

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,14 +1,59 @@
 import React, {Component} from 'react';
+import InstaService from '../services/myservice';
 
 export default class Notes extends Component {
+    InstaService = new InstaService();
     state = {
         notes: [],
     }
+
+    componentDidMount() {
+        this.updateNotes();
+    }
+
+    updateNotes() {
+        this.InstaService.getAllNotes()
+        .then(this.onNotesLoaded)
+        .catch(this.onError)
+    }
+
+    onNotesLoaded = (notes) => {
+        this.setState({
+            notes,
+            error: false,
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map(note => {
+            const {text, id} = note;
+
+            return (
+                <div key={id} className="post">
+                    <p>{text}</p>
+                </div>
+            )
+        })
+    }
     
     render() {
-        return(
-            <div>
-                <p>Hello World</p>
+        const {error, notes} = this.state;
+
+        if(error) {
+            return (
+                <p>Error</p>
+            )
+        }
+        const items = this.renderItems(notes);
+        return (
+            <div className="left">
+                {items}
             </div>
         )
     }
