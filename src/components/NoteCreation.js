89c2465aa0700c4d5f0d13f538cc6ee3e0054105
Kirill <kiril.borushko@gmail.com>
2019-06-14 18:11:22 +0300

    added an event handler to add data to json file

--- src/components/NoteCreation.js
+++ src/components/NoteCreation.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
+import InstaService from '../services/myservice';
 
 export default class NoteCreation extends Component {
+    InstaService = new InstaService();
     state = {
         text: '',
+        error: false,
     }
 
     setText = (e) => {
         this.setState({
             text: e.target.value
         })
+    }
+
+    createNote = () => {
+        let note = {
+            text: this.state.text,
+        }
+        
+        this.InstaService.postNote('notes', note)
+        .then(this.onPosted)
+        .catch(this.onError)
+    }
+
+    onPosted = (resp) => {
+        this.setState({text: ''}, console.log(this.state))
+    }
+
+    onError = () => {
+        this.setState({error: true}, console.log(this.state))
     }
 
     render() {
@@ -21,7 +42,8 @@ export default class NoteCreation extends Component {
                     onChange={this.setText}>
                 </input>
                 <span>
-                    <button>
+                    <button
+                        onClick={this.createNote}>
                         Add Note
                     </button>
                 </span>
